1ada92fb74b94ca6557e6e286602f1e2f3e56063
Josh Jarvis <jmj3886@rit.edu>
2021-02-01 09:18:05 -0500

    Updated to Use Plex Web API

--- PlexMovieCompare.py
+++ PlexMovieCompare.py
@@ -26,18 +26,30 @@ class CommandLineParser(object):
     def __init__(self):
         self._arg_parser = argparse.ArgumentParser(
             description="This tool Generates Movie Listings from a Plex "
-            "directory and can also compare them for differences between two "
+            "directory and compares them for differences between two "
             "Plex servers.",
             conflict_handler="resolve",
         )
         self._arg_parser.add_argument(
-            "-c",
-            "-C",
-            "--compare",
-            nargs=2,
-            metavar=('serverFile', 'localFile'),
-            help="Compares movie listing '<SERVER_FILE>' to '<LOCAL_FILE>' and "
-                 "identifies the differences.",
+            "remote_address",
+            nargs=1,
+            type=str,
+            metavar='<REMOTE_SERVER_ADDRESS>',
+            help="The IP address of the remote Plex server.",
+        )
+        self._arg_parser.add_argument(
+            "local_address",
+            nargs=1,
+            type=str,
+            metavar='<LOCAL_SERVER_ADDRESS>',
+            help="The IP address of the local Plex server.",
+        )
+        self._arg_parser.add_argument(
+            "api_token",
+            nargs=1,
+            type=str,
+            metavar='<API_TOKEN>',
+            help="The API token for connecting to Plex Web API.",
         )
         
     def parse_info(self, args=None):
@@ -51,60 +63,40 @@ class CommandLineParser(object):
         :rtype: Namespace
         """
         return self._arg_parser.parse_args(args)
-        
-        
-def plex_create_listing():
-    extensions = [".mp4", ".avi", ".m4v", ".mkv", ".mov", ".avchd", ".webm"]
-    movies = []
-    for cur_dir, dirs, files in os.walk("."):
-        for file in files:
-            ext = os.path.splitext(file)[-1].lower()
-            if ext in extensions:
-                movies.append({"title":os.path.splitext(file)[0], "location":os.path.basename(cur_dir)})
-                
-    with open('PlexMovieListing.txt', 'w') as listing_file:
-        for movie in sorted(movies, key=lambda i: i['title']):
-            print("%s :- %s" % (movie['title'], movie['location']), file=listing_file)
-            
-            
-def plex_compare(server_listing, local_listing):
+
+    
+def plex_compare(remote_listing, local_listing):
     with open('PlexMovieDifferences.txt', 'w') as diff_file:
-        server_movies = []
-        local_movies = []
         max_location_len = 0
         max_title_len = 0
-        with open(server_listing, 'r')as server_file:
-            for movie in server_file:
-                server_movies.append({'title':movie.split(" :- ")[0], 'location':movie.split(" :- ")[1].strip()})
-                if len(server_movies[-1]['title']) > max_title_len:
-                    max_title_len = len(server_movies[-1]['title'])
-                if len(server_movies[-1]['location']) > max_location_len:
-                    max_location_len = len(server_movies[-1]['location'])
-        with open(local_listing, 'r')as local_file:
-            for movie in local_file:
-                local_movies.append({'title':movie.split(" :- ")[0], 'location':movie.split(" :- ")[1].strip()})
-                if len(local_movies[-1]['title']) > max_title_len:
-                    max_title_len = len(local_movies[-1]['title'])
-                if len(local_movies[-1]['location']) > max_location_len:
-                    max_location_len = len(local_movies[-1]['location'])
-        for server_movie in server_movies:
+        for movie in remote_listing:
+            if len(movie['Title']) > max_title_len:
+                max_title_len = len(movie['Title'])
+            if len(movie['Library']) > max_location_len:
+                max_location_len = len(movie['Library'])
+        for movie in local_listing:
+            if len(movie['Title']) > max_title_len:
+                max_title_len = len(movie['Title'])
+            if len(movie['Library']) > max_location_len:
+                max_location_len = len(movie['Library'])
+        for server_movie in remote_listing:
             difference = True
-            title = server_movie['title']
-            location = "Move to Plex local at %s" % server_movie['location']
-            for local_movie in local_movies:
-                if server_movie['title'] == local_movie['title']:
+            title = server_movie['Title']
+            location = "Move to Plex local at %s" % server_movie['Library']
+            for local_movie in local_listing:
+                if server_movie['Title'] == local_movie['Title']:
                     difference = False
-                    if server_movie['location'] != local_movie['location']:
+                    if server_movie['Library'] != local_movie['Library']:
                         difference = True
-                        location = ("%-"+str(max_location_len)+"s -> %s") % (local_movie['location'], server_movie['location'])
+                        location = ("%-"+str(max_location_len)+"s -> %s") % (local_movie['Library'], server_movie['Library'])
             if difference:
                 print(("%-"+str(max_title_len)+"s :- %s") % (title, location), file=diff_file)
-        for local_movie in local_movies:
+        for local_movie in local_listing:
             difference = True
-            title = local_movie['title']
-            location = "Move to Plex server at %s" % local_movie['location']
-            for server_movie in server_movies:
-                if local_movie['title'] == server_movie['title']:
+            title = local_movie['Title']
+            location = "Move to Plex server at %s" % local_movie['Library']
+            for server_movie in remote_listing:
+                if local_movie['Title'] == server_movie['Title']:
                     difference = False
             if difference:
                 print(("%-"+str(max_title_len)+"s :- %s") % (title, location), file=diff_file)
@@ -126,20 +118,27 @@ def get_plex_library_content(plex_address, plex_token, libraries):
         movies_json = xmltodict.parse(req.text)
         library["Movies"] = []
         for movie in library_json["MediaContainer"]["Video"]:
-            library["Movies"].append({"Title":movie})
-        with open("Test.txt", "w") as test_file:
-            json.dump(xmltodict.parse(req.text), test_file, indent=4)
+            library["Movies"].append({"Title":movie["@title"]})
 
-
+            
+def generate_movie_listings(remote_address, local_address, plex_token):
+    remote_listing = []
+    remote_libraries = get_plex_libraries(remote_address, plex_token)
+    get_plex_library_content(remote_address, plex_token, libraries)
+    
+    local_listing = []
+    local_libraries = get_plex_libraries(local_address, plex_token)
+    get_plex_library_content(local_address, plex_token, libraries)
+    
+    for library in remote_libraries:
+        for movie in library['Movies']:
+            remote_listing.append({"Title":movie["Title"], "Library":library["Title"]})
+    for library in local_libraries:
+        for movie in library['Movies']:
+            local_listing.append({"Title":movie["Title"], "Library":library["Title"]})
+    return remote_listng, local_listing
+    
 if __name__ == "__main__":
     args = CommandLineParser().parse_info()
-    libraries = get_plex_libraries()
-    get_plex_library_content(libraries)
-    '''
-    if args.compare is not None:
-        server_listing, local_listing = args.compare
-        plex_compare(server_listing, local_listing)
-    else:
-        plex_create_listing()
-    '''
-            
+    plex_compare(generate_movie_listing(args.remote_address, args.local_address, args.api_token))
+    
